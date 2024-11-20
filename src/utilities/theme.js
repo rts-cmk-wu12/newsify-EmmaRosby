9a926c67a88adfbd04bbd7675c572de73282a2b4
EmmaRosby <U2457534@rts-365.dk>
2024-11-20 13:18:47 +0100

    finish dark mode

--- src/utilities/theme.js
+++ src/utilities/theme.js
@@ -1,13 +1,28 @@
 const background = document.querySelector('body');
-const fonts = document.querySelectorAll('.dark-mode-font');
+const fonts = document.querySelectorAll('.dark-mode-font'); 
+const imagesDark = document.querySelectorAll('.dark-mode-img');
+const imagesLight = document.querySelectorAll('.light-mode-img')
 
+// On page load, get the theme from localStorage
 const savedTheme = localStorage.getItem('theme'); 
-let isDark = savedTheme === 'dark'; 
+let isDark = savedTheme === 'dark'; // Set the initial state based on localStorage
 
+function toggleImages(showDark) {
+    imagesDark.forEach(img => {
+        img.style.display = showDark ? "block" : "none"; // Show dark mode images
+    });
+    imagesLight.forEach(img => {
+        img.style.display = showDark ? "none" : "block"; // Show light mode images
+    });
+}
+
+// Apply the saved theme
 if (isDark) {
     background.style.backgroundColor = '#222222';
     fonts.forEach(font => font.style.color = '#D9D9D9');
+    toggleImages(true)
 } else {
     background.style.backgroundColor = '#FFFFFF';
     fonts.forEach(font => font.style.color = '#404040');
+    toggleImages(false)
 }
