bada40ee754b81c201d1c3247555c2e784f6ec51
EmmaRosby <U2457534@rts-365.dk>
2024-11-22 09:23:13 +0100

    remove white space

--- src/home.js
+++ src/home.js
@@ -1,3 +1,2 @@
 import './utilities/home-articles';
 import './utilities/save-article';
-
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import './scss/style.scss';
 import './utilities/theme';
-
 import './utilities/refresh-api';
-
